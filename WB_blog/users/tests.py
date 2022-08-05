8cd7b034336995e93cdeb39e21696d382858fbe5
MasterMind7777777 <go.go.nest.nest@gmail.com>
2022-08-05 19:45:30 +0300

    test for sign up and log in

--- WB_blog/users/tests.py
+++ WB_blog/users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class TestUserAuth(APITestCase):
+
+    def test_sign_up(self):
+        data = {'username': 'test_user', 'password': 'Qdwq1234'}
+        response = self.client.post('/api/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_log_in(self):
+        data = {'username': 'test_user', 'password': 'Qdwq1234'}
+        self.client.post('/api/users/', data)
+        response = self.client.post('/api/auth/token/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    
