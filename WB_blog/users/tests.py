021b4b0d6257c18784000dd67bd6ddebbd7e6dab
MasterMind7777777 <go.go.nest.nest@gmail.com>
2022-08-08 18:26:21 +0300

    clean up structure

--- WB_blog/users/tests.py
+++ WB_blog/users/tests.py
@@ -1,5 +1,10 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 from rest_framework import status
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class TestUserAuth(APITestCase):
 
@@ -12,6 +17,17 @@ class TestUserAuth(APITestCase):
         data = {'username': 'test_user', 'password': 'Qdwq1234'}
         self.client.post('/api/users/', data)
         response = self.client.post('/api/auth/token/login/', data)
+        print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
-    
+
+# class TestUserMange(APITestCase):
+
+#     def setUp(self):
+#         client = APIClient()
+#         client.login(username='lauren', password='secret')
+#         token = Token.objects.get(user__username='lauren')
+#         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+#     def test_log_out(self):
+#         response = self.client.post('/api/auth/token/logout/')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
